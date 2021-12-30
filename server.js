fe532b1bac23970dad91b00f000465eabeac2b49
WccDave <dlloyd51@wccnet.edu>
2021-12-30 17:08:44 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const result = require('dotenv').config()
+
+
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+
+
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log("connected to mongoose"))
+
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/landing', { articles: articles })
+})
+
+app.get('/add', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
